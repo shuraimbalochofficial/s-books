d4f5567ba2f28d41b86cc039aa08527fb28a08d7
shuraimbalochofficial <shuraimbalochofficial@gmail.com>
2026-02-19 08:00:27 +0500

    Worked little bit on Navbar + installed Shadcn

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav>
+      <div className="">
+        <div className="">
+          <Link href="/">SBooks</Link>
+          <div></div>
+          {/* npx shadcn@latest init */}
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
