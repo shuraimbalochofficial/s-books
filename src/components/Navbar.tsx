f0213170638b51ac12e0ba2dd06e99cdb87fb524
shuraimbalochofficial <shuraimbalochofficial@gmail.com>
2026-02-20 16:33:23 +0500

    Buidling Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,17 @@
 import Link from "next/link";
 import React from "react";
 import { Button } from "./ui/button";
+import { BookKey } from "lucide-react";
 
 function Navbar() {
   return (
-    <nav className="border-b">
-      <div className="">
-        <div className="">
-          <Link href="/">SBooks</Link>
+    <nav className="border-b border-[#DAD3C8] bg-[#FCFAF7]/50 backdrop-blur-xs sticky top-0 z-50 px-4">
+      <div className="mx-auto">
+        <div className="flex items-center justify-between h-16">
+          <Link href="/" className="flex items-center gap-2">
+            <BookKey className="w-6 h-6" />
+            SBooks
+          </Link>
           <div>
             <Button>
               <Link href="/">Feed</Link>
