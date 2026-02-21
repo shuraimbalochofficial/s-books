3e50981d3184f65de97065add18872f8a618e775
shuraimbalochofficial <shuraimbalochofficial@gmail.com>
2026-02-21 17:46:20 +0500

    Learning & creating Add form Page

--- src/components/add-book/AddBookForm.tsx
+++ src/components/add-book/AddBookForm.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Card } from "../ui/card";
 import { Label } from "../ui/label";
+import { Input } from "../ui/input";
 
 const AddBookForm = () => {
   return (
@@ -8,7 +9,29 @@ const AddBookForm = () => {
       <Card className="p-8">
         <form>
           <div>
-            <Label>Book Title*</Label>
+            <Label htmlFor="title" className="font-semibold text-lg">
+              Book Title*
+            </Label>
+            <Input
+              id="title"
+              name="title"
+              placeholder="Enter the book title"
+              required
+              className="h-12 text-base!"
+            />
+          </div>
+          <div>
+            <Label htmlFor="author" className="font-semibold text-lg">
+              Author *
+            </Label>
+
+            <Input
+              id="author"
+              name="author"
+              placeholder="Enter the author's name"
+              required
+              className="h-12 text-base!"
+            />
           </div>
         </form>
       </Card>
