cb3dfc5c81b2aa24560530ad2b546fa7f4e7fe0b
shuraimbalochofficial <shuraimbalochofficial@gmail.com>
2026-02-24 09:22:18 +0500

    Add files via upload

--- src/app/api/books/route.ts
+++ src/app/api/books/route.ts
@@ -1,4 +1,5 @@
-import { auth } from "@clerk/nextjs/server";
+import { connectToDatabase } from "@/lib/connectToDB";
+import { auth, currentUser } from "@clerk/nextjs/server";
 
 //Adding a book to the DataBase
 export async function POST(req: Request) {
@@ -6,7 +7,14 @@ export async function POST(req: Request) {
     const { isAuthenticated } = await auth();
     if (!isAuthenticated) {
       return Response.json({ error: "UnAuthorised" }, { status: 401 });
+
+      await connectToDatabase();
+
+      const user = await currentUser();
+
+      const formData = await req.formData();
+
+      const title = formData.get("")?.toString() || "";
     }
   } catch (error) {}
 }
-
