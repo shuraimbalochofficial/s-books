677be24aa5e5c8aa3658cdb6d156d6c95970dc69
shuraimbalochofficial <shuraimbalochofficial@gmail.com>
2026-02-27 06:46:04 +0500

    Add files via upload

--- src/components/home/RecentlyAdded.tsx
+++ src/components/home/RecentlyAdded.tsx
@@ -1,6 +1,9 @@
 "use client";
 import axios from "axios";
+import { Loader2 } from "lucide-react";
 import React, { useEffect, useState } from "react";
+import { Button } from "../ui/button";
+import Link from "next/link";
 
 // interface Book{
 
@@ -11,12 +14,35 @@ const RecentlyAdded = () => {
   useEffect(() => {
     const fetchRecentBooks = async () => {
       try {
-        const response = await axios.get("")
-      }catch(error){}
-    }
-  }, [])
+        const response = await axios.get("/api/books");
+        const data = response.data;
+        setRecentBooks(data.books);
+      } catch (error) {
+        console.log("Error fetching recent books:", error);
+      }
+    };
 
-  return <div>Recently Added</div>;
+    fetchRecentBooks();
+  }, []);
+
+  if (recentBooks.length === 0) {
+    return (
+      <div className="py-5 px-5 min-h-screen flex items-center justify-center text-foreground">
+        <Loader2 className="animate-spin mr-1" /> ...Loading Books
+      </div>
+    );
+  }
+
+  return (
+    <section>
+      <div className="">
+        <h2>Recently Added</h2>
+        <Button variant="outline" asChild>
+          <Link href="/explore">View All</Link>
+        </Button>
+      </div>
+    </section>
+  );
 };
 
 export default RecentlyAdded;
