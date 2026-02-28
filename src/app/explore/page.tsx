d01ccde2955f8cfd755cdb896fc2a71ac1980e17
shuraimbalochofficial <shuraimbalochofficial@gmail.com>
2026-02-28 09:52:39 +0500

    Add files via upload

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -1,6 +1,8 @@
+"use client";
 import { Button } from "@/components/ui/button";
+import axios from "axios";
 import { Funnel } from "lucide-react";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const popularGenres = [
   "Classic",
@@ -17,13 +19,38 @@ const popularGenres = [
 
 const page = () => {
   const [selectedGenre, setSelectedGenre] = useState("");
+  const [books, setBooks] = useState<Array<Book>>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  const handleGenreClick = (genre: string) => {
+    setSelectedGenre(genre);
+  };
+
+  useEffect(() => {
+    const fetchBooks = async () => {
+      setLoading(true);
+      try {
+        const response = await axios.get("/api/books");
+        const data = response.data;
+        setBooks(data.books);
+      } catch (error) {
+        console.error("Error fetching books:", error);
+        setBooks([]);
+      } finally {
+        setLoading(false);
+      }
+
+      fetchBooks();
+    };
+  }, []);
+
   return (
     <div className="py-5 px-5">
       <h2 className="text-5xl! md:text-3xl font-bold text-foreground">
         Explore Our Collection
       </h2>
       <p className="text-base text-[#847062] leading-relaxed">
-        Browse through (x) literary treasures
+        Browse through {books.length} literary treasures
       </p>
 
       <div className="mt-8 flex items-center text-[#847062]">
@@ -31,7 +58,7 @@ const page = () => {
         <p>Filter By Genre</p>
       </div>
 
-      <div>
+      <div className="mt-4 flex flex-wrap gap-2">
         {popularGenres.map((genre) => (
           <Button
             key={genre}
@@ -45,6 +72,7 @@ const page = () => {
           </Button>
         ))}
       </div>
+      <p>Showing {books.length} Books</p>
     </div>
   );
 };
